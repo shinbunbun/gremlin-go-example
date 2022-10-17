84f0eb9b7afd89413e94c882f8711eb99927a9ad
shinbunbun <34409044+shinbunbun@users.noreply.github.com>
2022-10-17 07:22:13 +0000

    コマンドライン引数に対応

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"gremlin-go-example/create"
 	"gremlin-go-example/search"
 	"log"
+	"os"
 
 	gremlingo "github.com/apache/tinkerpop/gremlin-go/v3/driver"
 )
@@ -19,34 +20,45 @@ func main() {
 	defer driverRemoteConnection.Close()
 	g := gremlingo.Traversal_().WithRemote(driverRemoteConnection)
 
-	// vertex作成
-	vertexes, err := create.CreateVertexes(g)
-	if err != nil {
-		log.Fatalln(err.Error())
+	if len(os.Args) < 2 {
+		fmt.Println("コマンドライン引数が足りません")
+		os.Exit(1)
 	}
 
-	fmt.Println()
+	arg := os.Args[1]
+	if arg == "create" {
+		// vertex作成
+		vertexes, err := create.CreateVertexes(g)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
 
-	// edge作成
-	err = create.CreateEdges(g, vertexes)
-	if err != nil {
-		log.Fatalln(err.Error())
+		fmt.Println()
+
+		// edge作成
+		err = create.CreateEdges(g, vertexes)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+	} else if arg == "search" {
+
+		fmt.Println("\nグラフに登録されているユーザーの名前一覧を取得")
+		search.GetAllUsersName(g)
+
+		fmt.Println("\nユーザー1に紐づくuserの名前一覧を取得")
+		search.GetRelatedUsersName(g, "ユーザー1")
+
+		fmt.Println("\nユーザー7がフォローしているユーザーの投稿を取得")
+		search.GetFollowUsersPosts(g, "ユーザー7")
+
+		fmt.Println("\nユーザー3から枝方向に再帰的に探索し、ユーザーの名前一覧を取得")
+		search.GetUsersRecursive(g, "ユーザー3")
+
+		fmt.Println("\n再帰で2階層下（ユーザー3がフォローしているユーザーがフォローしているユーザー）まで取得")
+		search.GetUsersRecursive2Step(g, "ユーザー3")
+
+		fmt.Println()
+	} else {
+		fmt.Println("対応していないコマンドライン引数です")
 	}
-
-	fmt.Println("\nグラフに登録されているユーザーの名前一覧を取得")
-	search.GetAllUsersName(g)
-
-	fmt.Println("\nユーザー1に紐づくuserの名前一覧を取得")
-	search.GetRelatedUsersName(g, "ユーザー1")
-
-	fmt.Println("\nユーザー7がフォローしているユーザーの投稿を取得")
-	search.GetFollowUsersPosts(g, "ユーザー7")
-
-	fmt.Println("\nユーザー3から枝方向に再帰的に探索し、ユーザーの名前一覧を取得")
-	search.GetUsersRecursive(g, "ユーザー3")
-
-	fmt.Println("\n再帰で2階層下（ユーザー3がフォローしているユーザーがフォローしているユーザー）まで取得")
-	search.GetUsersRecursive2Step(g, "ユーザー3")
-
-	fmt.Println()
 }
